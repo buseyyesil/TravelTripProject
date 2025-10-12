f8e8466e7adf07791a6d6e94b2350541ec68e77c
buse yeşil <buseyesil55@gmail.com>
2025-10-12 17:36:54 +0300

    Güncel proje

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -9,20 +9,43 @@ namespace TravelTripProject.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: Blog
         Context c=new Context();
+
+        BlogYorum by = new BlogYorum();
+
         public ActionResult Index()
         {
-            var bloglar=c.Blogs.ToList();
-            return View(bloglar);
+            //var bloglar=c.Blogs.ToList();
+            by.Deger1=c.Blogs.ToList(); 
+            by.Deger3=c.Blogs.OrderByDescending(x => x.ID).Take(3).ToList(); 
+            return View(by);
         }
-        BlogYorum by=new BlogYorum();
+        
         public ActionResult BlogDetay(int id)
         {
             
             //var blogbul=c.Blogs.Where(x=>x.ID==ID).ToList();
-            by.Deger1=c.Blogs.Where(x=>x.ID==id).ToList();  
+            by.Deger1=c.Blogs.Where(x=>x.ID==id).ToList(); 
+            by.Deger2=c.Yorumlars.Where(x=>x.BlogId==id).ToList();  
             return View(by);  
+        }
+        //[HttpGet]
+        //public PartialViewResult()
+        //{
+        //    return PartialView();
+        //}
+        [HttpGet]
+        public PartialViewResult YorumYap(int id)
+        {
+            ViewBag.deger = id;
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult YorumYap(Yorumlar y)
+        {
+            c.Yorumlars.Add(y);
+            c.SaveChanges();
+            return PartialView();
         }
     }
 }
